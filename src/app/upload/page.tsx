082d5cacc7db504e4f733119abbba653d128f848
Zalcom53 <zalcom53@gmail.com>
2025-01-28 11:31:47 -0700

    029-1

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Upload = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-8">
+        Panel administrativo de muestras
+      </h1>
+      {/* Resto del contenido de tu componente */}
+    </div>
+  );
+};
+
+export default Upload;
